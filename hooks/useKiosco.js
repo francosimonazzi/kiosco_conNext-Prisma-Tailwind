0fa9303b3b16227b8dc0441fef915b27ac750047
franco <francosimonazzi@hotmail.com>
2022-06-14 15:12:21 -0300

    hola

---
+++ hooks/useKiosco.js
@@ -0,0 +1,11 @@
+import { useContext } from "react";
+import KioscoContext from "../context/KioscoProvider";
+
+
+const useKiosco = () => {
+    return useContext(KioscoContext);
+};
+
+
+
+export default useKiosco;
